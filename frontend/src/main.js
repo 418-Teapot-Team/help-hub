95fff542fbd33c4e605839dffd5ca083beea7c04
Kolia913 <mynew1mail470@gmail.com>
2024-04-25 02:06:06 +0300

    feat: added custom http client. Installed all required plugins

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,9 +6,19 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+import VeeValidatePlugin from '@/includes/validation';
+import Toast from '@/includes/toast';
+import VueSelect from '@/includes/v-select';
+import Maska from '@/includes/maska';
+
 const app = createApp(App);
 
+// includes
 app.use(createPinia());
 app.use(router);
+app.use(VeeValidatePlugin);
+app.use(Toast);
+app.use(VueSelect);
+app.use(Maska);
 
 app.mount('#app');
