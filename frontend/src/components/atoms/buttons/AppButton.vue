7a052b14679c9939aa334acb90241b7dac849dfd
Pasha1007 <61791504+Pasha1007@users.noreply.github.com>
2024-04-25 03:02:07 +0300

    updated button component

--- frontend/src/components/atoms/buttons/AppButton.vue
+++ frontend/src/components/atoms/buttons/AppButton.vue
@@ -1,6 +1,11 @@
 <template>
   <button
-    class="w-full h-full p-2 font-semibold bg-primary bg-opacity-85 text-xl text-white"
+    :class="[
+      'w-full h-full p-2 font-semibold text-xl',
+      buttonStyle === 'default'
+        ? 'bg-primary bg-opacity-85 text-white'
+        : 'bg-transparent border border-primary text-primary',
+    ]"
     :type="type"
     @click="onClick"
   >
@@ -8,14 +13,29 @@
   </button>
 </template>
 
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+  text: String,
+  type: {
+    type: String,
+    default: 'primary',
+  },
+  buttonStyle: {
+    type: String,
+    default: 'default',
+  },
+});
+
+const { emit } = defineEmits(['onClick']);
+
+const onClick = () => {
+  emit('onClick');
+};
+</script>
 <script>
 export default {
   name: 'AppButton',
-  props: ['text', 'type'],
-  methods: {
-    onClick() {
-      this.$emit('onClick');
-    },
-  },
 };
 </script>
