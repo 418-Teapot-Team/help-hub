0e9a625ed511f08bf6efd36d49f2732ebbedc65e
Alex <oleksii.uzhva.pz.2021@lpnu.ua>
2024-04-27 16:55:08 +0500

    refactor: separate component for searchbar + minor folder structure change

--- frontend/src/views/FindVolunteerView.vue
+++ frontend/src/views/FindVolunteerView.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="bg-section-secondary w-screen h-screen">
     <Filters />
-    <div class="flex justify-center">
+    <div class="flex justify-center h-screen">
       <FindVolunteerFeed />
     </div>
   </div>
 </template>
 
 <script setup>
-import FindVolunteerFeed from '@/components/feed/FindVolunteerFeed.vue';
+import FindVolunteerFeed from '@/components/feed/feed/FindVolunteerFeed.vue';
 import Filters from '@/components/feed/filters/Filters.vue';
 </script>
