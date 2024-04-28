07f7cccadf532986f431391e1844cbd2f9e6f255
Alex <oleksii.uzhva.pz.2021@lpnu.ua>
2024-04-28 13:53:20 +0300

    feat: responcive behaviour for cards

--- frontend/src/components/home/stats/StatCard.vue
+++ frontend/src/components/home/stats/StatCard.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="mx-9 pt-5 bg-white shadow-md m-3 w-56">
+  <div class="mx-9 pt-5 bg-white shadow-md m-3 w-28 h-56 md:w-48">
     <div class="grid grid-cols-1 place-items-center">
       <div class="md:flex-shrink-0 pt-5">
-        <img class="object-contain h-20 w-20" :src="image" />
+        <img class="object-contain h-16" :src="image" />
       </div>
       <div class="p-8 text-center text-wrap">
         <div class="uppercase tracking-wide text-2xl text-primary font-bold">{{ stat }}</div>
